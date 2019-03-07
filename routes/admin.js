c01cce9b281dbb96777572b555f49c659f060c96
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-03-08 00:51:31 +0100

    pug structure, dynamic content

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,9 @@ const products = []
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {title: 'Some text'})
+//z render lahko uporabimo t.i. templating engine; prvi argument pove datoteko katero renderamo, drugi
+//pa je objekt, ki vključuje dynamic content
 });
 
 // /admin/add-product => POST
