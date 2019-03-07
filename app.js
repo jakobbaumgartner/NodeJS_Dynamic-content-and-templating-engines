73e5e245bedcf58ecb1a166375ac34be35aeffbb
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-03-07 20:40:25 +0100

    pug_first update

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+//Uporabimo templating engine, preko express (v node.js bolj zapleteno):
+app.set('view engine', 'pug');
+app.set('views', 'views');
+//app.set omogoča nam nastavitev globalnih vrednosti, nevem ... glej API
+
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,13 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const adminData = require('./admin')
+
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  console.log(adminData.products);
+  res.render('shop')
+  // Render metoda bo uporabila default templating engine (tooo smo definirali z app.set!! aaaahaa :D ) in vrnila izdelek.
+  //Ker so vsi views v views mapi (spet app.set) ne potrebujemo poti
 });
 
 module.exports = router;
